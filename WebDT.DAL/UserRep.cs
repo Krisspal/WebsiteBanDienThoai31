436a9df82e84167230286b67759fc7ff22e8b179
Krisspal <hphuc2133@gmail.com>
2024-05-23 13:57:22 +0700

    Full CRUD

--- WebDT.DAL/UserRep.cs
+++ WebDT.DAL/UserRep.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using WebDT.DAL.Models;
+using WebDT.Common.Rsp;
 
 namespace WebDT.DAL
 {
@@ -16,6 +17,27 @@ namespace WebDT.DAL
             var res = All.FirstOrDefault(u => u.UserId==id);
             return res;
         }
-
+        //public SingleRsp CreateUser(User user)
+        //{
+        //    var res = new SingleRsp();
+        //    using (var context = new QuanLyBanDienThoaiContext())
+        //    {
+        //        using (var tran = context.Database.BeginTransaction())
+        //        {
+        //            try
+        //            {
+        //                var p = context.Users.Add(user);
+        //                context.SaveChanges();
+        //                tran.Commit();
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                tran.Rollback();
+        //                res.SetError(ex.StackTrace);
+        //            }
+        //        }
+        //    }
+        //    return res;
+        //}
     }
 }
--- WebDT.Web/Controllers/UserController.cs
+++ WebDT.Web/Controllers/UserController.cs
@@ -16,11 +16,44 @@ namespace WebDT.Web.Controllers
             userSvc = new UserSvc();
         }
 
-        [HttpPost("Lay User theo ID")]
-        public IActionResult GetUserByID([FromBody] SimpleReq simpleReq)
+        [HttpPost]
+        [Route("{id}")]
+        public IActionResult GetUserByID(int id)
         {
             var res = new SingleRsp();
+            res = userSvc.Read(id);
             return Ok(res);
         }
+
+        [HttpGet("GetAllUser")]
+        public IActionResult GetAllUser()
+        {
+            var res = new SingleRsp();
+            res.Data = userSvc.All;
+            return Ok(res);
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult CreateUser(UserReq userReq)
+        {
+            var res = userSvc.CreateUser(userReq);
+            return Ok(res);
+        }
+
+        [HttpPut("UpdateUser")]
+        public IActionResult UpdateUser(string id, UserReq userReq)
+        {
+            var res = userSvc.UpdateUser(id, userReq);
+            return Ok(res);
+        }
+
+        [HttpDelete("DeleteUser")]
+        public IActionResult DeleteUser(int id)
+        {
+            var res = userSvc.DeleteUser(id);
+            return Ok(res);
+        }
+
+
     }
 }
